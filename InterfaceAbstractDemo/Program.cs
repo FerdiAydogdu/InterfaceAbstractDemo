fe0d908f194adbaf808f308edc3840a14191d24b
ferdi <aydogduferdi14@gmail.com>
2022-10-25 18:40:22 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(2000, 6, 4), FirstName = "Ferdi", LastName = "Aydoğdu", NationalityId = "29884248308" });
+
+            Console.ReadLine();
+        }
+    }
+}
